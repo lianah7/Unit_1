796ecb9b8895d9737b7d44318c870fc169fec9f8
liana <hilllia@ssfs.org>
2019-09-11 08:54:43 -0400

    Made some shapes

---
+++ shapes.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.penup(400)
+turtle.pendown
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+
+turtle.exitonclick()
