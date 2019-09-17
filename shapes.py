750fef6686f5e3845c1ff08873d4caebc2949e61
liana <hilllia@ssfs.org>
2019-09-17 14:15:32 -0400

    Added functions

--- shapes.py
+++ shapes.py
@@ -1,10 +1,20 @@
 import turtle
 
 turtle.speed(8)
-# makes a triangle
-for x in range(3):
-    turtle.forward(100)
-    turtle.left(120)
+
+# function for making a square
+def draw_square():
+    for x in range(4):
+        turtle.fd(100)
+        turtle.right(90)
+
+
+# function for making a triangle
+def draw_triangle():
+    for x in range(3):
+        turtle.forward(100)
+        turtle.left(120)
+
 
 turtle.penup()
 turtle.left(90)
@@ -19,6 +29,8 @@ turtle.penup()
 turtle.forward(140)
 turtle.right(160)
 turtle.pendown()
+
+# makes a pentagon
 turtle.forward(90)
 turtle.left(72)
 turtle.forward(90)
@@ -32,6 +44,7 @@ turtle.forward(90)
 turtle.penup()
 turtle.forward(300)
 turtle.pendown()
+
 turtle.circle(60)
 turtle.left(90)
 
@@ -40,6 +53,7 @@ turtle.forward(300)
 turtle.left(50)
 turtle.pendown()
 
+# makes a square
 for x in range(5):
     turtle.forward(70)
     turtle.left(144)
@@ -48,14 +62,11 @@ turtle.penup()
 turtle.forward(200)
 turtle.pendown()
 
-for x in range(4):
-    turtle.forward(90)
-    turtle.left(90)
+# following lines make many squares
+draw_square()
 turtle.right(20)
 
-for x in range(4):
-    turtle.forward(90)
-    turtle.left(90)
+draw_square()
 turtle.right(20)
 
 for x in range(4):
@@ -94,5 +105,6 @@ turtle.penup()
 turtle.forward(140)
 turtle.pendown()
 turtle.seth(180)
+turtle.end_fill()
 
 turtle.exitonclick()
